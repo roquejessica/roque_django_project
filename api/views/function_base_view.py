78411c88be5fe4ed58a5cf9f5ec3a3032e87c0a3
Jessica Roque <jessicaroque363@gmail.com>
2025-10-14 19:13:19 +0800

    added POST method

--- api/views/function_base_view.py
+++ api/views/function_base_view.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 
 # Create your views here.
 def studentView(request):
