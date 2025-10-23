80027bb8df31529323407471843db37588ce88e9
Jessica Roque <jessicaroque363@gmail.com>
2025-10-23 18:31:03 +0800

    explored mixins with employee

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from api.views import student_function_base_view, employee_function_base_view, student_class_base_view, employee_class_base_view
+from api.views import student_function_base_view, employee_function_base_view, student_class_base_view, employee_class_base_view, mixins_employee
 
 
 urlpatterns = [
@@ -15,4 +15,7 @@ urlpatterns = [
 
     path('cbv-employees/', employee_class_base_view.Employees.as_view()),
     path('cbv-employee/<int:pk>', employee_class_base_view.EmployeeDetail.as_view()),
+
+    path('mixins-employees/', mixins_employee.Employees.as_view()),
+    path('mixins-employee-detail/<int:pk>/', mixins_employee.EmployeeDetail.as_view()),
 ]
---
+++ api/views/mixins_employee.py
@@ -0,0 +1,27 @@
+from api.serializer import EmployeeSerializer
+from rest_framework import mixins, generics
+from employees.models import Employee
+
+
+class Employees(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+    def get(self, request):
+        return self.list(request)
+    
+    def post(self, request):
+        return self.create(request)
+    
+class EmployeeDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+    def get (self, request, pk):
+        return self.retrieve(request, pk)
+    
+    def put(self, request, pk):
+        return self.update(request, pk)
+    
+    def delete(self, request, pk):
+        return self.destroy(request, pk)
