dcbde76407a519e2a083bf3b1da260dbe0a76eb0
Jessica Roque <jessicaroque363@gmail.com>
2025-10-28 18:34:56 +0800

    added create, retrieve, update, delete in viewset

--- api/views/employee_viewset.py
+++ api/views/employee_viewset.py
@@ -9,3 +9,28 @@ class Employees(viewsets.ViewSet):
         queryset = Employee.objects.all()
         serializer = EmployeeSerializer(queryset, many=True)
         return Response(serializer.data)
+    
+    def create(self, request):
+        serializer = EmployeeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors)
+    
+    def retrieve(self, request, pk=None):
+        employee = get_object_or_404(Employee, pk=pk)
+        serializer = EmployeeSerializer(employee)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def update(self, request, pk=None):
+        employee = get_object_or_404(Employee, pk=pk)
+        serializer = EmployeeSerializer(employee, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+    
+    def delete(self, request, pk=None):
+        employee = get_object_or_404(Employee, pk=pk)
+        employee.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
